6ac766ee89db2b082b87dd362275282b40bf644d
My name <me@example.com>
2021-10-17 20:33:45 +0200

    solve key and NotFound Page issues

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import NavBar from './navBar';
 
 class NotFound extends React.Component {
     render() { 
         return <div>
+            <NavBar/>
             <h1>Not Found 404</h1>
         </div>;
     }
--- src/components/questionPage.js
+++ src/components/questionPage.js
@@ -11,12 +11,12 @@ class QuestionPage extends React.Component {
     handleAnsweredQuestion=()=>{
         const passedId=this.props.match.params.id
         const questions = this.props.questions
-        const avatar = this.props.users[questions[passedId].author].avatarURL
-        const name = this.props.users[questions[passedId].author].name
         const question = questions[passedId]
-        if(!question){
+        if((!questions[passedId])){
             return  <Redirect to='/not-found'/>
         }else{
+            const avatar = this.props.users[questions[passedId].author].avatarURL
+            const name = this.props.users[questions[passedId].author].name
             if(question.optionOne.votes.includes(this.props.authedUser) || question.optionTwo.votes.includes(this.props.authedUser)){
                 let selected =''
                 if(question.optionOne.votes.includes(this.props.authedUser)){
@@ -37,12 +37,6 @@ class QuestionPage extends React.Component {
 
 
     render() { 
-        const passedId=this.props.match.params.id
-        const questions = this.props.questions
-        const avatar = this.props.users[questions[passedId].author].avatarURL
-        const name = this.props.users[questions[passedId].author].name
-
-
         return (
         <div >
             <h3>Pull Page</h3>
